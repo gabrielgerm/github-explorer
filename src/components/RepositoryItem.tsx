723b885241625099c0a00cd3a2921dbd4172dc8f
Gabriel Germano De Oliveira <gabriel-germano333@hotmail.com>
2022-01-12 20:56:29 -0300

    first commit

---
+++ src/components/RepositoryItem.tsx
@@ -0,0 +1,20 @@
+interface RepositoryitemProps {
+    repository: {
+        name: string;
+        description: string;
+        html_url: string;
+    }
+}
+
+export function Repositoryitem(props:RepositoryitemProps) {
+    return (
+        <li>
+            <strong>{props.repository.name}</strong>
+            <p>{props.repository.description}</p>
+
+            <a href={props.repository.html_url}>
+                Acessar repositórios
+            </a>
+        </li>   
+    )
+}
